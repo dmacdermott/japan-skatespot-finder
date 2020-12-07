f69621a7ccf785ca3f818f64de17134de8770a6a
dmacdermott <damianmacdermott@gmail.com>
2020-12-07 18:11:33 +0900

    basic get and post fuctions done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import database from './fire'
+import "firebase/database"
+
 
 function App() {
+
+
+  function writeNewSpot( name, rating, typeArr) {
+   database.ref('spots/').push({
+      spot_name: name,
+      rating: rating,
+      type : typeArr,
+    });
+  }
+  writeNewSpot("test spot", "3", ["manual"])
+
+  var allSpots = database.ref("spots")
+  allSpots.on("value", (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+
+  })
+ 
   return (
+
     <div className="App ">
-       <h1>HELLO DAMIAN!</h1>
+       <h1>HELLO peopel</h1>
     </div>
   );
 }
---
+++ src/fire.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app"
+import "firebase/database"
+
+ // Set the configuration for your app
+  // TODO: Replace with your project's config object
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCrxeVCqvJpFuGbkMIRBvNgb-aW7vu0D_8",
+    authDomain: "japan-skate-spot-finder.firebaseapp.com",
+    databaseURL: "https://japan-skate-spot-finder-default-rtdb.firebaseio.com",
+    projectId: "japan-skate-spot-finder",
+    storageBucket: "japan-skate-spot-finder.appspot.com",
+    messagingSenderId: "173154016077",
+    appId: "1:173154016077:web:747a9c2bf2e9261b46f504",
+    measurementId: "G-5J9E0PRPET"
+  };
+
+  var fire = firebase.initializeApp(firebaseConfig);
+
+  export default fire.database()
